5a326ad5057c48c78c0b1dde9a119a82c1dbe91b
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-09 09:36:09 +0100

    Added view and controller for materials

---
+++ src/main/java/es/upm/miw/sportscentre/controllers/MaterialController.java
@@ -0,0 +1,35 @@
+package es.upm.miw.sportscentre.controllers;
+
+import es.upm.miw.sportscentre.models.Material;
+import es.upm.miw.sportscentre.models.daos.MaterialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class MaterialController {
+
+  @Autowired
+  private MaterialRepository materialRepository ;
+
+  public List<Material> findAll() {
+    return materialRepository.findAll();
+  }
+
+  public Material findById(String id) {
+    return materialRepository.findById(id).orElse(null);
+  }
+
+  public Material save(Material Material) {
+    return materialRepository.save(Material);
+  }
+
+  public void deleteById(String id) {
+    materialRepository.deleteById(id);
+  }
+
+  public void deleteAll() {
+    materialRepository.deleteAll();
+  }
+
+}
--- src/main/java/es/upm/miw/sportscentre/models/Material.java
+++ src/main/java/es/upm/miw/sportscentre/models/Material.java
@@ -1,2 +1,21 @@
-package es.upm.miw.sportscentre.models;public class Material {
+package es.upm.miw.sportscentre.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "materials")
+public class Material {
+  @Id
+  private String id;
+  private String name;
+  private String description;
+  private Integer quantity;
 }
