5ddeed16292145121776100db6b4233805139e8e
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-16 23:37:11 +0100

    Working notices CRUD

--- src/main/java/es/upm/miw/sportscentre/controllers/NoticeController.java
+++ src/main/java/es/upm/miw/sportscentre/controllers/NoticeController.java
@@ -1,9 +1,11 @@
 package es.upm.miw.sportscentre.controllers;
 import es.upm.miw.sportscentre.models.Notice;
 import es.upm.miw.sportscentre.models.daos.NoticeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class NoticeController {
@@ -20,7 +22,17 @@ public class NoticeController {
   }
 
   public Notice save(Notice notice) {
-    return noticeRepository.save(notice);
+    return noticeRepository.save(Notice.builder()
+        .title(notice.getTitle())
+        .body(notice.getBody())
+        .dateTime(notice.getDateTime())
+        .build());
+  }
+
+  public Notice update(String id, Notice notice) {
+    Notice existingNotice = this.findById(id);
+    BeanUtils.copyProperties(notice, existingNotice);
+    return this.noticeRepository.save(existingNotice);
   }
 
   public void deleteById(String id) {
