28f8366b1e5f6ef33e83bdbc6832d343de081c76
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-17 11:11:17 +0100

    Temporaly excluyed database seeder from testing

--- src/main/java/es/upm/miw/sportscentre/models/daos/DatabaseSeederDev.java
+++ src/main/java/es/upm/miw/sportscentre/models/daos/DatabaseSeederDev.java
@@ -4,11 +4,13 @@ import es.upm.miw.sportscentre.models.Installation;
 import es.upm.miw.sportscentre.models.Material;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Profile("!test")
 @Service
 public class DatabaseSeederDev {
   private final MaterialRepository materialRepository;
