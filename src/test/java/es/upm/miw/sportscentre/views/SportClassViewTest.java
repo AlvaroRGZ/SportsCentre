820572f3049f92345d3e3c72be7a20044e94c209
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-23 14:22:08 +0100

    Added sportclass view tests

--- src/test/java/es/upm/miw/sportscentre/views/SportClassViewTest.java
+++ src/test/java/es/upm/miw/sportscentre/views/SportClassViewTest.java
@@ -1,4 +1,99 @@
 package es.upm.miw.sportscentre.views;
 
+import es.upm.miw.sportscentre.controllers.SportClassController;
+import es.upm.miw.sportscentre.models.SportClass;
+import es.upm.miw.sportscentre.views.dtos.SportClassCreationDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SportClassViewTest {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Autowired
+  private SportClassController sportClassController;
+
+  @Test
+  void testGetAllSportClasses() {
+    ResponseEntity<SportClass[]> response = restTemplate.getForEntity("/sportclasses", SportClass[].class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    SportClass[] sportClasses = response.getBody();
+    assertNotNull(sportClasses);
+    assertTrue(sportClasses.length > 0);
+  }
+
+  @Test
+  void testGetSportClassById() {
+    ResponseEntity<SportClass> response = restTemplate.getForEntity("/sportclasses/1", SportClass.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    SportClass sportClass = response.getBody();
+    assertNotNull(sportClass);
+    assertEquals("Yoga Class", sportClass.getTitle());
+  }
+
+  @Test
+  void testGetSportClassesByUserEmail() {
+    ResponseEntity<SportClass[]> response = restTemplate.getForEntity("/sportclasses/user/a@gmail.com", SportClass[].class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    SportClass[] sportClasses = response.getBody();
+    assertNotNull(sportClasses);
+    assertTrue(sportClasses.length > 0);
+  }
+
+  @Test
+  void testCreateSportClass() {
+    SportClassCreationDto newSportClass = SportClassCreationDto.builder()
+        .title("Pilates Class")
+        .installation("1")
+        .places(20)
+        .build();
+    ResponseEntity<SportClass> response = restTemplate.postForEntity("/sportclasses", newSportClass, SportClass.class);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    SportClass savedSportClass = response.getBody();
+    assertNotNull(savedSportClass);
+    assertEquals("Pilates Class", savedSportClass.getTitle());
+  }
+
+  @Test
+  void testAddAssistantToSportClass() {
+    restTemplate.put("/sportclasses/1/addAssistant?email=admin@gmail.com", null);
+    SportClass updatedSportClass = sportClassController.findById("1");
+    assertNotNull(updatedSportClass);
+    assertTrue(updatedSportClass.getAssistants().stream().anyMatch(a -> a.getEmail().equals("admin@gmail.com")));
+  }
+
+  @Test
+  void testRemoveAssistantFromSportClass() {
+    restTemplate.put("/sportclasses/1/addAssistant?email=admin@gmail.com", null);
+    restTemplate.put("/sportclasses/1/removeAssistant?email=admin@gmail.com", null);
+    SportClass updatedSportClass = sportClassController.findById("1");
+    assertNotNull(updatedSportClass);
+    assertFalse(updatedSportClass.getAssistants().stream().anyMatch(a -> a.getEmail().equals("admin@gmail.com")));
+  }
+
+  @Test
+  void testDeleteSportClass() {
+    SportClassCreationDto newSportClass = SportClassCreationDto.builder()
+        .title("Class to Delete")
+        .installation("1")
+        .places(20)
+        .build();
+    SportClass savedSportClass = sportClassController.save(newSportClass);
+    restTemplate.delete("/sportclasses/" + savedSportClass.getId());
+    Exception exception = assertThrows(RuntimeException.class, () -> {
+      sportClassController.findById(savedSportClass.getId());
+    });
+    assertEquals("Sportclass not found", exception.getMessage());
+  }
 }
