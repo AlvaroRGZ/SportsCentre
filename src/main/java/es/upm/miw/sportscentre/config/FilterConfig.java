9120fd8f7b9e49b56597dfdf7a1f7af072ce335c
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-25 12:15:47 +0100

    Separated filtering protection with profiles

--- src/main/java/es/upm/miw/sportscentre/config/FilterConfig.java
+++ src/main/java/es/upm/miw/sportscentre/config/FilterConfig.java
@@ -3,7 +3,9 @@ package es.upm.miw.sportscentre.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("Prod")
 @Configuration
 public class FilterConfig {
   @Bean
