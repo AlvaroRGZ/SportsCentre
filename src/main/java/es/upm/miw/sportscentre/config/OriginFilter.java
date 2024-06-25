68844489429ceba3a71e5fa69c7d592f9421c5c1
Alvaro Rodriguez Gomez <alu0101362953@ull.edu.es>
2024-06-25 11:50:33 +0100

    Opened filter config for tests requests

--- src/main/java/es/upm/miw/sportscentre/config/OriginFilter.java
+++ src/main/java/es/upm/miw/sportscentre/config/OriginFilter.java
@@ -8,38 +8,45 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-@Component
-public class OriginFilter implements Filter {
+@Configuration
+public class FilterConfig {
 
   private static final String ALLOWED_ORIGIN = "http://localhost:4200";
   private static final String SERVER_ADDRESS = "http://localhost:8080";
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-  }
+  @Bean(name = "customOriginFilter")
+  public Filter originFilter() {
+    return new Filter() {
 
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    HttpServletResponse httpResponse = (HttpServletResponse) response;
+      @Override
+      public void init(FilterConfig filterConfig) throws ServletException {
+      }
 
-    String origin = httpRequest.getHeader("Origin");
-    String referer = httpRequest.getHeader("Referer");
+      @Override
+      public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+          throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-    if ((ALLOWED_ORIGIN.equals(origin) || SERVER_ADDRESS.equals(origin)) &&
-        (referer == null || referer.startsWith(ALLOWED_ORIGIN) || referer.startsWith(SERVER_ADDRESS))) {
-      chain.doFilter(request, response);
-    } else {
-      httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden: Invalid Origin or Referer");
-    }
-  }
+        String origin = httpRequest.getHeader("Origin");
+        String referer = httpRequest.getHeader("Referer");
 
-  @Override
-  public void destroy() {
+        if ((ALLOWED_ORIGIN.equals(origin) || SERVER_ADDRESS.equals(origin)) &&
+            (referer == null || referer.startsWith(ALLOWED_ORIGIN) || referer.startsWith(SERVER_ADDRESS))) {
+          chain.doFilter(request, response);
+        } else {
+          httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden: Invalid Origin or Referer");
+        }
+      }
+
+      @Override
+      public void destroy() {
+      }
+    };
   }
 }
